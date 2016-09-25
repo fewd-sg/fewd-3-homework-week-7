c52e12c9398c3526e4ad0df40d38a9d74f5b9c7a
nervousgeek <demented182@hotmail.com>
2016-09-26 01:30:20 +0800

    NEW MODIFIED FUNGSHAAANS!!!

--- farid/script/city.js
+++ farid/script/city.js
@@ -1,42 +1,45 @@
 var city = ["NYC","SF","LA","ATX","SYD"]
 
-
+//add into dropdowns
 city.forEach(function(element, index){
   $( "#city-type" ).append("<option>"+city[index]+"</option>");
 });
 
-
+//new shorter function after changing css classes!!
 $( "#city-type" ).change(function() {
+  var input=$( "#city-type" ).val()
 
-  citySelect = $( "#city-type" ).val();
+  if ($( "#city-type" ).val() === input){
+    $("body").removeClass("SF LA ATX SYD NYC");
+    $("body").addClass(input);
+  }
 
-  if (citySelect === "NYC"){
-    removeCity();
+/*old code that works too but am keeping it because
+/*of assignment requirements for if/else if
+  if ($( "#city-type" ).val() === "NYC"){
+    $("body").removeClass("SF LA ATX SYD NYC");
     $("body").addClass("NYC");
   }
 
-  else if (citySelect === "SF"){
-    removeCity();
+  else if ($( "#city-type" ).val() === "SF"){
+    $("body").removeClass("SF LA ATX SYD NYC");
     $("body").addClass("SF");
   }
 
-  else if (citySelect === "LA"){
-    removeCity();
+  else if ($( "#city-type" ).val() === "LA"){
+    $("body").removeClass("SF LA ATX SYD NYC");
     $("body").addClass("LA");
   }
 
-  else if (citySelect === "ATX"){
-    removeCity();
+  else if ($( "#city-type" ).val() === "ATX"){
+    $("body").removeClass("SF LA ATX SYD NYC");
     $("body").addClass("ATX");
   }
 
-  else if (citySelect === "SYD"){
-    removeCity();
+  else if ($( "#city-type" ).val() === "SYD"){
+    $("body").removeClass("SF LA ATX SYD NYC");
     $("body").addClass("SYD")
   }
+  */
 
 });
-
-function removeCity(){
-  $("body").removeClass("SF LA ATX SYD NYC");
-}
